2871bd57d4e117d3ed0c838a0e4ede057e78a077
Prashant Kumar Yadav <kartik224blue@gmail.com>
2022-01-26 02:30:36 +0530

    adding toogle button in navbar to change graph size

--- script.js
+++ script.js
@@ -33,6 +33,39 @@ var inject = (parentDiv) => {
   tag_solve_div.innerHTML = child_node_tag_solve;
   tag_solve_div.className = "css-lw67gk";
   parentDiv.insertBefore(tag_solve_div, parentDiv.childNodes[2]);
+
+
+  
+  var nav_bar_left_section=document.getElementsByClassName("navbar-right-container__COIx")[0];
+  var toggle_button=`<label class="switch">
+                  <input id="toogleBtn" type="checkbox"  >
+                  <span class="slider round"></span>
+                </label>`;
+  var parent_div_toggle_button=document.createElement("div");
+  parent_div_toggle_button.className="nav-item-container__16kF";
+  parent_div_toggle_button.innerHTML=toggle_button;
+  nav_bar_left_section.insertBefore(parent_div_toggle_button, nav_bar_left_section.childNodes[0]);
+  let toogle_button_elemnt = document.getElementById("toogleBtn");
+  toogle_button_elemnt.onchange=()=>{
+    let parentDiv_account_section=document.getElementsByClassName("profile-content-wrapper__3CQt")[0];
+    let parentDiv_account_section_child=document.getElementsByClassName("profile-side-bar__3r8O")[0];
+    let parentDiv_account_section_second_child=document.getElementsByClassName("profile-content__3PmZ")[0];
+    console.log(toogle_button_elemnt.checked);
+    if(toogle_button_elemnt.checked==true){
+      parentDiv_account_section.classList.remove("parentDiv_display");
+      parentDiv_account_section.classList.add("parentDiv");
+      parentDiv_account_section_child.classList.add("parentDiv_child");
+      parentDiv_account_section_second_child.classList.add("parentDiv_second_child");
+      
+    }else{
+      parentDiv_account_section.classList.remove("parentDiv");
+      parentDiv_account_section.classList.add("parentDiv_display");
+      parentDiv_account_section_child.classList.remove("parentDiv_child");
+      parentDiv_account_section_second_child.classList.remove("parentDiv_second_child");
+
+    }
+  };
+  
   let total=0;
   userData.stat_status_pairs.forEach((element) => {
     const id = element.stat.question_id.toString(); 
@@ -72,7 +105,9 @@ var inject = (parentDiv) => {
     }
   })
 
+ 
   
+
 
   var Chart = require("chart.js");
   var ctx = document.getElementById("myChart");
@@ -136,3 +171,7 @@ async function getApiData(url){
      const myJSON = JSON.stringify(data);
      localStorage.setItem('userData', myJSON);
 }
+
+// var tooglClasses=(e)=>{
+  
+// }
