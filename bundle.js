ce9721ae8c868a3a94cd7c49572c33d086a865b7
Prashant Kumar Yadav <kartik224blue@gmail.com>
2022-02-08 17:13:01 +0530

    Added contest history table

--- bundle.js
+++ bundle.js
@@ -1,9 +1,9 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const url="https://leetcode.com/api/problems/algorithms/";
+const url = "https://leetcode.com/api/problems/algorithms/";
+
 var inject = (parentDiv) => {
   clearInterval(myTimer);
-  
-  
+
   var child_node_tag_solve = `
     <div class="ant-card css-8kh3x9 ant-card-small">
     <div class="ant-card-head">
@@ -15,29 +15,29 @@ var inject = (parentDiv) => {
     <div id="bar-graph" style="    margin-bottom: 23px;"><canvas id="myChart" ></canvas></div>
      </div>
  </div>`;
-  
 
   var tablink = location.toString();
-  tablink = tablink.replace("https://leetcode.com/","");
-  let username=tablink.slice(0, -1);
+  tablink = tablink.replace("https://leetcode.com/", "");
+  let username = tablink.slice(0, -1);
 
-  
   getApiData(url);
 
-  let tagData_With_NumberOf_Questions=[];
-  let total_tags=[];
-  let sleepCounter=1;
+  let tagData_With_NumberOf_Questions = [];
+  let total_tags = [];
+  let sleepCounter = 1;
 
-  if(localStorage.getItem("userData")==null&&localStorage.getItem("error_fetching")==null){
-    localStorage.setItem('error_fetching', 1);
+  if (
+    localStorage.getItem("userData") == null &&
+    localStorage.getItem("error_fetching") == null
+  ) {
+    localStorage.setItem("error_fetching", 1);
     setTimeout(() => {
-      
       location.reload();
     }, 2000);
   }
-  let userData=JSON.parse(localStorage.getItem("userData"));
+  let userData = JSON.parse(localStorage.getItem("userData"));
 
-  if(userData.user_name!==username){
+  if (userData.user_name && userData.user_name !== username) {
     return;
   }
   var tag_solve_div = document.createElement("div");
@@ -45,88 +45,93 @@ var inject = (parentDiv) => {
   tag_solve_div.className = "css-lw67gk";
   parentDiv.insertBefore(tag_solve_div, parentDiv.childNodes[2]);
 
-
-  
-  var nav_bar_left_section=document.getElementsByClassName("navbar-right-container__COIx")[0];
-  var toggle_button=`<label class="switch">
+  var nav_bar_left_section = document.getElementsByClassName(
+    "navbar-right-container__COIx"
+  )[0];
+  var toggle_button = `<label class="switch">
                   <input id="toogleBtn" type="checkbox"  >
                   <span class="slider round"></span>
                 </label>`;
-  var parent_div_toggle_button=document.createElement("div");
-  parent_div_toggle_button.className="nav-item-container__16kF";
-  parent_div_toggle_button.innerHTML=toggle_button;
-  nav_bar_left_section.insertBefore(parent_div_toggle_button, nav_bar_left_section.childNodes[0]);
+  var parent_div_toggle_button = document.createElement("div");
+  parent_div_toggle_button.className = "nav-item-container__16kF";
+  parent_div_toggle_button.innerHTML = toggle_button;
+  nav_bar_left_section.insertBefore(
+    parent_div_toggle_button,
+    nav_bar_left_section.childNodes[0]
+  );
   let toogle_button_elemnt = document.getElementById("toogleBtn");
-  toogle_button_elemnt.onchange=()=>{
-    let parentDiv_account_section=document.getElementsByClassName("profile-content-wrapper__3CQt")[0];
-    let parentDiv_account_section_child=document.getElementsByClassName("profile-side-bar__3r8O")[0];
-    let parentDiv_account_section_second_child=document.getElementsByClassName("profile-content__3PmZ")[0];
-    console.log(toogle_button_elemnt.checked);
-    if(toogle_button_elemnt.checked==true){
+  toogle_button_elemnt.onchange = () => {
+    let parentDiv_account_section = document.getElementsByClassName(
+      "profile-content-wrapper__3CQt"
+    )[0];
+    let parentDiv_account_section_child = document.getElementsByClassName(
+      "profile-side-bar__3r8O"
+    )[0];
+    let parentDiv_account_section_second_child =
+      document.getElementsByClassName("profile-content__3PmZ")[0];
+    // console.log(toogle_button_elemnt.checked);
+    if (toogle_button_elemnt.checked == true) {
       parentDiv_account_section.classList.remove("parentDiv_display");
       parentDiv_account_section.classList.add("parentDiv");
       parentDiv_account_section_child.classList.add("parentDiv_child");
-      parentDiv_account_section_second_child.classList.add("parentDiv_second_child");
-      
-    }else{
+      parentDiv_account_section_second_child.classList.add(
+        "parentDiv_second_child"
+      );
+    } else {
       parentDiv_account_section.classList.remove("parentDiv");
       parentDiv_account_section.classList.add("parentDiv_display");
       parentDiv_account_section_child.classList.remove("parentDiv_child");
-      parentDiv_account_section_second_child.classList.remove("parentDiv_second_child");
-
+      parentDiv_account_section_second_child.classList.remove(
+        "parentDiv_second_child"
+      );
     }
+    
   };
-  
-  let total=0;
+
+  let total = 0;
   userData.stat_status_pairs.forEach((element) => {
-    const id = element.stat.question_id.toString(); 
-    if (id in allTagsData&&element.status === "ac"){
+    const id = element.stat.question_id.toString();
+    if (id in allTagsData && element.status === "ac") {
       total++;
       const topicTags = allTagsData[id].tags;
-      topicTags.forEach((tag)=>{
+      topicTags.forEach((tag) => {
         total_tags.push(tag);
-      })
+      });
     }
-  
-  })
+  });
   allTags.forEach((tag) => {
-    let count=0;
-    total_tags.forEach((tagName)=>{
-      if(tagName===tag) count++;
-    
-    })
-    if(count!==0){
-        tagData_With_NumberOf_Questions.push({tag:tag, count:count});
+    let count = 0;
+    total_tags.forEach((tagName) => {
+      if (tagName === tag) count++;
+    });
+    if (count !== 0) {
+      tagData_With_NumberOf_Questions.push({ tag: tag, count: count });
     }
-  })
-  
-  const background_color_for_bars=[];
-  const barLabels_tagCount=[];
-  const barLabels_tagName=[];
-  
-  tagData_With_NumberOf_Questions.forEach((tagDetails)=>{
+  });
+
+  const background_color_for_bars = [];
+  const barLabels_tagCount = [];
+  const barLabels_tagName = [];
+
+  tagData_With_NumberOf_Questions.forEach((tagDetails) => {
     let tagName = tagDetails.tag;
     let tagCount = tagDetails.count;
     barLabels_tagCount.push(tagCount);
     barLabels_tagName.push(tagName);
-    for(let idx in allTags){
-      if(allTags[idx]===tagName){
+    for (let idx in allTags) {
+      if (allTags[idx] === tagName) {
         background_color_for_bars.push(allTagColor[idx]);
       }
     }
-  })
-
- 
-  
-
+  });
 
   var Chart = require("chart.js");
   var ctx = document.getElementById("myChart");
- 
+
   var myChart = new Chart(ctx, {
     type: "bar",
     data: {
-      labels:barLabels_tagName ,
+      labels: barLabels_tagName,
       datasets: [
         {
           data: barLabels_tagCount,
@@ -149,45 +154,85 @@ var inject = (parentDiv) => {
           grid: {
             display: false,
           },
-        }
+        },
       },
-    }
+    },
   });
 
+  // insert contest history table
 
+  let contestTableHtml = `<table id="contest_table">
+<thead>
+  <tr>
+    <th>CONTEST</th>
+    <th>SCORE</th>
+    <th>RANK</th>
+    <th>TOTAL</th>
+  </tr>
+</thead><thead>
+</thead><tbody>
   
+</tbody>
+</table>`;
+var contest_table = document.createElement("div");
+contest_table.innerHTML = contestTableHtml;
+contest_table.className = "ant-card css-8kh3x9 ant-card-small";
 
+let contestArray=localStorage.getItem("all_contest_details");
+contestArray=JSON.parse(contestArray);
+let contestArrayId=localStorage.getItem("all_contest_id");
+contestArrayId=JSON.parse(contestArrayId);
+
+let leftSideHtml=document.getElementsByClassName("profile-side-bar__3r8O")[0];
+leftSideHtml.append(contest_table);
+if(contestArrayId.length!=0){
+  console.log(all_contest_array.length);
+
+  let t=document.getElementById("contest_table").tBodies[0]
+  contestArrayId.forEach(element => {
+    contestArray.forEach(user=>{
+    if(element==user.contest_id){
+      let row=document.createElement("tr");
+      let column=`<td><a class="col" href=https://leetcode.com/contest/${user.contest_name}>${user.formatedContestName}</a></td>
+      <td class="col-color">${user.score}</td>
+      <td class="col-color">${user.rank}</td>
+      <td class="col-color">${user.total_participat}</td>`
+      row.innerHTML=column;
+        t.append(
+          row
+        )
+      }
+    })
+  });
+}
 
 };
 
-  var myTimer = setInterval(() => {
-    setTimeout(() => {
-      clearInterval(myTimer);
-    }, 10000);
-    let parentDiv = document.getElementsByClassName("profile-content__3PmZ")[0];
-    if (parentDiv !== undefined ) {
-      inject(parentDiv);
-    }
-  }, 500);
-  
+ 
 
+var myTimer = setInterval(() => {
+  setTimeout(() => {
+    clearInterval(myTimer);
+  }, 10000);
+  let parentDiv = document.getElementsByClassName("profile-content__3PmZ")[0];
+  if (parentDiv !== undefined) {
+    inject(parentDiv);
+  }
+}, 500);
 
+async function getApiData(url) {
+  const response = await fetch(url);
 
-
-async function getApiData(url){
-     const response = await fetch(url);
-     
-     // Storing data in form of JSON
-     var data = await response.json();
-     const myJSON = JSON.stringify(data);
-     localStorage.setItem('userData', myJSON);
+  // Storing data in form of JSON
+  var data = await response.json();
+  const myJSON = JSON.stringify(data);
+  localStorage.setItem("userData", myJSON);
 }
 
-// var tooglClasses=(e)=>{
-  
-// }
+
 
 // browserify script.js -o bundle.js
+
 },{"chart.js":2}],2:[function(require,module,exports){
 /*!
  * Chart.js v3.7.0
---
+++ src/component/fetchUserRankAndContest_Details.js
@@ -0,0 +1,70 @@
+let all_contest_array=[];
+let all_contest=[];
+let all_contest_id=[];
+
+function fetchAllContest(){
+  for (let i = 0; i < localStorage.length; i++)  {      
+   let key = localStorage.key(i);
+   if(key.includes("contest_ac_status:")){
+     let contest_name=key.split("contest_ac_status:")[1];
+     if(!contest_name.includes("-updated-time")){
+      all_contest.push(contest_name);
+     }
+   }
+  }
+}
+
+
+function formatContestName(contest_name){
+  let arr=contest_name.split("-");
+  let ans="";
+  arr.forEach(str=>{
+    ans+=str.charAt(0).toUpperCase() + str.slice(1)+" ";
+  })
+  return ans;
+}
+function findDetailsOfContest(contest_name){
+  
+  let contestName=formatContestName(contest_name);
+  let page_no = 1;
+  let link_for_total_particpant = `https://leetcode.com/contest/api/ranking/${contest_name}/?pagination=${page_no}&region=global`;
+  let link_for_personal_rank = `https://leetcode.com/contest/api/myranking/${contest_name}/?region=global`;
+  let total_participation;
+    let fetchRes = fetch(link_for_total_particpant);
+    fetchRes
+    .then((res) => res.json())
+    .then((user) => {
+      total_participation=user.user_num;
+      let fetchuser_rank = fetch(link_for_personal_rank);
+      fetchuser_rank
+      .then((res) => res.json())
+      .then((user) => {
+        all_contest_array.push({
+          contest_id:user.my_rank.contest_id,
+          formatedContestName:contestName,
+          contest_name:contest_name,
+          rank:user.my_rank.rank+1,
+          score:user.my_rank.score,
+          total_participat:total_participation
+        })
+        all_contest_id.push(user.my_rank.contest_id);
+      });
+    });
+}
+
+function fill_all_contest_array(){
+  fetchAllContest();
+
+  all_contest.forEach(element => {
+    findDetailsOfContest(element);
+  });
+
+}
+
+fill_all_contest_array();
+setTimeout(() => {
+  all_contest_id.sort();
+  all_contest_id.reverse();
+  localStorage.setItem("all_contest_id",JSON.stringify(all_contest_id));
+  localStorage.setItem("all_contest_details",JSON.stringify(all_contest_array));
+}, 2000);
